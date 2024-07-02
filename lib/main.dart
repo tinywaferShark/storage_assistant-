1d65dc2b7283919ddd752aa81bbab1a3a8361412
shark <sharksharkgo@outlook.com>
2024-07-02 23:33:35 +0800

    fix:fix theme config bug

--- lib/main.dart
+++ lib/main.dart
@@ -5,19 +5,33 @@ void main() {
   runApp(MyApp());
 }
 
-class MyApp extends StatelessWidget {
+class MyApp extends StatefulWidget {
+  @override
+  _MyAppState createState() => _MyAppState();
+}
+
+class _MyAppState extends State<MyApp> {
+  ThemeMode _themeMode = ThemeMode.system;
+
+  void _updateTheme(ThemeMode themeMode) {
+    setState(() {
+      _themeMode = themeMode;
+    });
+  }
+
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
       title: '收纳助理', // 修改应用的标题为中文
       theme: ThemeData(
         primarySwatch: Colors.blue,
+        brightness: Brightness.light,
       ),
-      home: HomePage(
-        onThemeChanged: (ThemeMode themeMode) {
-          // 这里可以根据主题变更执行操作
-        },
+      darkTheme: ThemeData(
+        brightness: Brightness.dark,
       ),
+      themeMode: _themeMode,
+      home: HomePage(onThemeChanged: _updateTheme),
     );
   }
 }
