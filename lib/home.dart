295a612de26f5a3d902b09f4d6bc14cde42b640a
shark <sharksharkgo@outlook.com>
2024-07-02 23:58:54 +0800

    feat:added the add buttom function

--- lib/home.dart
+++ lib/home.dart
@@ -50,6 +50,17 @@ class _HomePageState extends State<HomePage> {
     });
   }
 
+  Widget _buildIconButton(IconData icon, String label) {
+    return Column(
+      mainAxisSize: MainAxisSize.min,
+      children: [
+        Icon(icon, size: 50),
+        SizedBox(height: 5),
+        Text(label),
+      ],
+    );
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -58,17 +69,53 @@ class _HomePageState extends State<HomePage> {
         centerTitle: true,
         actions: _selectedIndex == 0
             ? <Widget>[
-          IconButton(
-            icon: Icon(Icons.add),
-            iconSize: 36.0,
-            onPressed: () {
-              Navigator.push(
-                context,
-                MaterialPageRoute(builder: (context) => AddItemPage()),
-              );
-            },
-          ),
-        ]
+                IconButton(
+                  icon: Icon(Icons.add),
+                  iconSize: 36.0,
+                  onPressed: () {
+                    showModalBottomSheet(
+                      context: context,
+                      builder: (BuildContext context) {
+                        return Container(
+                          height: MediaQuery.of(context).size.height * 0.33, // 占据屏幕的三分之一
+                          padding: const EdgeInsets.all(16.0),
+                          child: Column(
+                            crossAxisAlignment: CrossAxisAlignment.stretch,
+                            children: [
+                              Text(
+                                '请选择导入方式',
+                                style: TextStyle(
+                                  fontSize: 20,
+                                  fontWeight: FontWeight.bold,
+                                ),
+                                textAlign: TextAlign.center,
+                              ),
+                              SizedBox(height: 20),
+                              ListTile(
+                                leading: Icon(Icons.input),
+                                title: Text('手动录入'),
+                                onTap: () {
+                                  Navigator.pop(context);
+                                  // 处理手动录入逻辑
+                                },
+                              ),
+                              SizedBox(height: 20),
+                              Row(
+                                mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+                                children: [
+                                  _buildIconButton(Icons.link, '链接导入'),
+                                  _buildIconButton(Icons.qr_code, '条码扫描'),
+                                  _buildIconButton(Icons.medical_services, '药品扫描'),
+                                ],
+                              ),
+                            ],
+                          ),
+                        );
+                      },
+                    );
+                  },
+                ),
+              ]
             : null,
       ),
       body: _pages[_selectedIndex],
